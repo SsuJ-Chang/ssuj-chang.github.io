165bb5b4423c6cabce19ae5521f043a19c1e4173
RJ Chang <loch0728@gmail.com>
2024-09-07 02:58:04 +0800

    Add markdown style for code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,7 +14,26 @@ module.exports = {
 			},
 			gridTemplateColumns: {
 				list: 'repeat(auto-fill, minmax(400px, max-content))'
-			}
+			},
+			typography: (theme) => ({
+        DEFAULT: {
+          css: {
+						code: {
+              backgroundColor: theme('colors.green.900 / 0.4'),  // 程式碼背景色
+              padding: '0.2rem 0.4rem',                  // 程式碼內距
+              borderRadius: '0.25rem',                   // 圓角效果
+              fontFamily: theme('fontFamily.mono'),      // 使用等寬字體
+              fontSize: '1.2rem',                      // 程式碼字體大小
+            },
+            'code::before': {
+              content: 'none',
+            },
+            'code::after': {
+              content: 'none',
+            },
+          },
+        },
+      }),
 		}
 	},
 	plugins: [require('@tailwindcss/typography')]
